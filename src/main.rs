093226936ba69a1e52d06a5eb03c0a44227fd33e
Aviral Srivastava <avi.srivastava254084@gmail.com>
2023-09-11 11:09:43 -0400

    wip: prepends IV instead of appending it

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let uuid4_api_key = Uuid::new_v4().to_string();
     println!("Generated UUID: {}", uuid4_api_key);
 
-    let key_str = "";
+    let key_str = std::env::var("ENCRYPTED_KEY").expect("ENCRYPTED_KEY not set");
     let key = hex::decode(key_str).expect("Failed to decode key");
     let key = Key::<Aes256Gcm>::from_slice(&key);
 
@@ -23,24 +23,24 @@ fn main() {
     let cipher = Aes256Gcm::new(&key);
     let ciphertext: Vec<u8> = cipher.encrypt(&nonce, uuid4_api_key.as_ref()).unwrap();
 
-    // Append nonce to ciphertext
-    let mut combined = ciphertext.clone();
+    // Construct the combined layout: nonce + ciphertext + auth tag
+    let mut combined = Vec::new();
     combined.extend_from_slice(&nonce);
+    combined.extend_from_slice(&ciphertext);
 
     println!(
-        "Combined (ciphertext + nonce): {:?}",
+        "Combined (nonce + ciphertext + auth tag): {:?}",
         hex::encode(&combined)
     );
 
     // Now let's simulate the decryption side:
 
-    // Split the ciphertext and nonce
-    let nonce_pos = combined.len() - 12;
-    let received_ciphertext = &combined[..nonce_pos];
-    let received_nonce = &combined[nonce_pos..];
+    // Split the nonce, ciphertext, and auth tag
+    let received_nonce = &combined[..12];
+    let received_ciphertext_with_tag = &combined[12..];
 
     let received_nonce = GenericArray::from_slice(received_nonce);
-    let plaintext = cipher.decrypt(received_nonce, received_ciphertext).unwrap();
+    let plaintext = cipher.decrypt(received_nonce, received_ciphertext_with_tag).unwrap();
     let plaintext_from_utf8 = std::str::from_utf8(&plaintext).unwrap();
 
     println!("Decrypted plaintext: {:?}", plaintext_from_utf8);
